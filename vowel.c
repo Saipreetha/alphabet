6d123d537dab1bd9551065acc5f3a7cbc6decf76
Saipreetha <saipreethaekb@gmail.com>
2016-12-23 11:39:38 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c;
+printf("\n enter the character");
+scanf("%c",&c);
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+{
+printf("\n it is a vowel");
+}
+else if(c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+{
+printf("\n it is a vowel");
+}
+else
+{
+printf("\n it is not a vowel");
+}
+}
